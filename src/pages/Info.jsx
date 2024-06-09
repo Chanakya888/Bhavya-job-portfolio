17ee3b7761870c9efb4238f831eaad08c17d35d5
Chan <samuraichanakya1@gmail.com>
2024-06-09 12:39:08 +0530

    feat: updated info page

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -7,77 +7,87 @@ import EmailIcon from "../assets/email-icon.svg";
 
 const Info = () => {
   return (
-    <div className="calc-height">
-      <div className=" px-3 pt-10 lg:pt-20 xl:pt-24">
-        <div className=" lg:flex lg:w-10/12 lg:space-x-12 lg:px-12 xl:space-x-16">
-          <div className="lg:w-2/5">
-            <img
-              className="h-full w-full object-cover"
-              src={ProfilePicture}
-              alt=""
-            />
-          </div>
-          <div className="flex flex-col justify-between pt-8 lg:w-3/5 lg:max-w-[457px] lg:pt-0">
-            <div>
-              {/* <p className="text-xl font-medium uppercase lg:text-2xl">
-                biographical note
-              </p> */}
-              <div className="p-container">
-                <p>
-                  I am a Computer Science Engineer and a UX Designer. I have
-                  around a year and half of professional experience and a formal
-                  design education from Srishti Manipal Institute of Art, Design
-                  and Technology (Bangalore, India). My personal goal as a
-                  designer is to explore as many facets of design and get to
-                  work around them. I really like the research part of any
-                  design process as it involves exploring and discovering new
-                  ideas.
-                </p>
-                <p>
-                  I am currently in a rabbit hole exploring the "drawing with
-                  code" part of design (p5.js) and working around how can this
-                  be intersected with Generative Art.
-                </p>
-                <p>
-                  I can bring in creative perspectives and ideas to any process,
-                  and I get real excited when I get to learn new methods or work
-                  around something new (whether it's digital interfaces,
-                  tangible materials or concepts).
-                </p>
-                <p>If you have any similar thoughts or ideas lets chat.</p>
-              </div>
+    <div className="h-screen">
+      <div className=" lg:calc-height px-6 pt-10 lg:grid lg:grid-cols-14 lg:pt-20 xl:pt-24">
+        <div className="lg:col-start-1 lg:col-end-5 lg:pl-10">
+          <img
+            className="h-full w-full object-cover lg:pb-1.5"
+            src={ProfilePicture}
+            alt=""
+          />
+        </div>
+        <div className="increase-p-size flex flex-col justify-between pt-8 lg:col-start-6 lg:col-end-14 lg:pt-0">
+          <div>
+            <div className="p-container">
+              <p>
+                In simple words: I design experiences through the medium of
+                digital interfaces. My approach to problem-solving is
+                multi-disciplinary: observing people, understanding spaces, and
+                engaging deeply in context. I am a Computer Science Engineer
+                with a Master's in{" "}
+                <span className="underline">Human-Centered Design.</span> My
+                journey in design began with a few Google searches during a
+                hackathon around the 6th semester of my engg. course, that led
+                me down an unending list of rabbit holes, each more fascinating
+                than the last. Since then, I have been deeply curious about
+                every conversation around design, behaviour patterns,
+                technology, humans, and their experiences.
+              </p>
+              <p>
+                Currently, I'm exploring drawing with code (p5.js), mixed media
+                animations, and how these intersect with Generative Art.
+              </p>
+              <p>
+                I bring creative perspectives and ideas to any process and
+                thrive on learning and gauging with new concepts and frameworks,
+                whether they involve tangible materials, digital products, or
+                research methodologies.
+              </p>
+              <p>If you have any similar thoughts or ideas, lets chat. </p>
             </div>
-            <div className="pt-3">
-              <Hrline />
-              <div className="space-y-1.5 pt-2.5">
-                <p>For projects/ inquires, please contact:</p>
-                <a href="mailto:dsg.bhavya@gmail.com">
-                  <div className="flex space-x-1.5 pt-3">
-                    <img
-                      className="w-[16px] object-contain"
-                      src={EmailIcon}
-                      alt=""
-                    />
-                    <p>dsg.bhavya@gmail.com</p>
-                  </div>
-                </a>
-              </div>
+          </div>
+          <div className="pt-3">
+            <Hrline />
+            <div className="space-y-1.5 pt-2.5">
+              <p>For projects/ inquires, please contact:</p>
+              <a href="mailto:dsg.bhavya@gmail.com">
+                <div className="flex space-x-1.5 ">
+                  <img
+                    className="w-[16px] object-contain"
+                    src={EmailIcon}
+                    alt=""
+                  />
+                  <p>dsg.bhavya@gmail.com</p>
+                </div>
+              </a>
             </div>
           </div>
         </div>
+        <div className="mt-20 space-y-1 pb-8 lg:col-start-6 lg:space-y-4">
+          <p className="add-circular-std text-xs font-medium">
+            LinkedIn:{" "}
+            <a
+              href="https://www.linkedin.com/in/bhavyagupta26/"
+              target="_blank"
+              rel="noreferrer"
+              className="underline"
+            >
+              @bhavyeahgupta
+            </a>
+          </p>
+          <p className="add-circular-std text-xs font-medium">
+            Instagram:{" "}
+            <a
+              href="https://www.instagram.com/bhavyeahgupta/?hl=en"
+              target="_blank"
+              rel="noreferrer"
+              className="underline"
+            >
+              @bhavyeahgupta
+            </a>
+          </p>
+        </div>
       </div>
-      {/* <div className="">
-        <Link to={"/"}>
-          <div className="flex w-full items-center space-x-3 pb-2 pl-3 pt-8 lg:pb-8 lg:pl-16 lg:pt-16 xl:pb-10 xl:pl-12 xl:pt-64">
-            <div>
-              <img src={GreenLeftArrow} alt="" />
-            </div>
-            <div>
-              <p className="text-green text-base lg:text-xl">Home</p>
-            </div>
-          </div>
-        </Link>
-      </div> */}
     </div>
   );
 };
